fb50e75080f46700201a2f88cbc2870360f2218b
Hemanth-Zero <hemanthsagar0111@gmail.com>
2026-04-01 18:08:12 +0000

    env added

--- Control/server.js
+++ Control/server.js
@@ -22,13 +22,14 @@ app.use("/", pageRoutes);
 
 //--------------------------------- DATABASE SETUP-------------------------
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',      
-  host: 'localhost',       
-  database: 'bookspace',    
-  password: 'mypostgres', 
-  port: 5432,              
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,            
 });
 
 pool.connect()
